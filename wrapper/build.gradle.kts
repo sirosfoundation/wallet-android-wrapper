f0a77c790b9f385dba43ed8b44611593ccefe3c3
Benjamin Erhart <berhart@netzarchitekten.com>
2025-11-24 17:40:24 +0100

    Removed outdated and now optional references to Java version and kotlinCompilerExtensionVersion.

--- wrapper/build.gradle.kts
+++ wrapper/build.gradle.kts
@@ -66,12 +66,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = "11"
     }
 
     buildFeatures {
@@ -80,7 +79,6 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
     }
 
     packaging {
