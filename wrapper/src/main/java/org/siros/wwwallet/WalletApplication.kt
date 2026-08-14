b525b5bc23bfffb3eb79d7d904cee3cd8b5baa07
Benjamin Erhart <berhart@netzarchitekten.com>
2026-08-14 14:36:16 +0200

    Added an uncaughtExceptionHandler to catch unhandled exceptions and log them to disk.

--- wrapper/src/main/java/org/siros/wwwallet/WalletApplication.kt
+++ wrapper/src/main/java/org/siros/wwwallet/WalletApplication.kt
@@ -1,6 +1,8 @@
 package org.siros.wwwallet
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.util.Log
 import org.siros.wwwallet.util.FileLoggingTree
 import timber.log.Timber
 
@@ -9,5 +11,22 @@ class WalletApplication : Application() {
         super.onCreate()
 
         Timber.plant(FileLoggingTree(this))
+
+        setupCrashHandler()
+    }
+
+    @SuppressLint("LogNotTimber")
+    private fun setupCrashHandler() {
+        val original = Thread.getDefaultUncaughtExceptionHandler()
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            try {
+                Timber.e(throwable, "FATAL CRASH in thread ${thread.name}: ${throwable.message}")
+            } catch (tr: Throwable) {
+                Log.e("CrashHandler", "Error in crash handler", tr)
+            } finally {
+                original?.uncaughtException(thread, throwable)
+            }
+        }
     }
 }
